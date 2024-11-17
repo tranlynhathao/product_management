a6218eeb9552df9e299473781137c06d00cb71cb
tranlynhathao <haotran04022005@gmail.com>
2024-11-17 10:54:30 +0700

    connect mongodb

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://haotran04022005:LdaX8GHe8ymyxdka@task-management.ixw2q.mongodb.net/?retryWrites=true&w=majority&appName=task-management",
-);
+// mongoose.connect(
+//   "mongodb+srv://haotran04022005:LdaX8GHe8ymyxdka@task-management.ixw2q.mongodb.net/?retryWrites=true&w=majority&appName=task-management",
+// );
+
+mongoose.connect("mongodb://127.0.0.1:27017/test");
 
 const Product = mongoose.model("Product", {
   title: String,
   price: String,
   thumnail: String,
+});
+
+const newProduct = new Product({
+  title: "apple",
+  price: "0.5",
+  thumnail:
+    "https://tranlynhathao.vercel.app/_next/image?url=https%3A%2F%2Fositcom.com%2Fstatic%2Fimages%2Fimages%2Fgit-blog-header_1.png&w=3840&q=75",
 });
 
 app.set("view engine", "pug");
@@ -32,7 +41,10 @@ app.get("/", (req, res) => {
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   console.log(products);
-  res.send("<h1>Product List</h1>");
+  res.render("products", {
+    titlePage: "Product List",
+    product: products,
+  });
 });
 
 app.get("/blog", (req, res) => {
