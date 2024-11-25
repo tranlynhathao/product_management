4735e155e696b28b05feba8f5fbf7e0399597abe
tranlynhathao <haotran04022005@gmail.com>
2024-11-25 12:09:42 +0700

    build layout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const database = require("./config/database");
+const systemConfig = require("./config/system");
 
 const routeClient = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
@@ -11,6 +12,9 @@ database.connect();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// app local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 
