d55df415f221200c1690adff3266310fe0ee6434
tranlynhathao <haotran04022005@gmail.com>
2024-11-27 16:47:28 +0700

    define http methods

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,3 +1,4 @@
+// [GET] /admin/products
 module.exports.index = (req, res) => {
   res.render("admin/pages/products/index", {
     pageTitle: "Product List",
--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,4 @@
+// [GET] /
 module.exports.index = function (req, res) {
   res.render("client/pages/home/index");
 };
