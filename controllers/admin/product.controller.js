7fb6f5e3c4a618b7e822ed463dfe1b021f105ecb
tranlynhathao <haotran04022005@gmail.com>
2024-11-29 10:11:36 +0700

    optimize filter

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,34 +1,11 @@
 const Product = require("../../models/product.model");
+const filterStatusHelper = require("../../helpers/filterStatus");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "All",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Active",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Inactive",
-      status: "inactive",
-      class: "",
-    },
-  ];
+  const filterStatus = filterStatusHelper(req.query);
 
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status,
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  console.log(filterStatus);
 
   let find = {
     deleted: false,
